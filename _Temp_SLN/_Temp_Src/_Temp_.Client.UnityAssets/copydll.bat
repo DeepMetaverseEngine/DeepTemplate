661a26fdf82bdfae55be8b06d9605d4560a866e0
wazazhang <wazazhang@gmail.com>
2026-08-28 19:08:08 +0800

    MyUnity_Data

--- _Temp_SLN/_Temp_Src/_Temp_.Client.UnityAssets/copydll.bat
+++ _Temp_SLN/_Temp_Src/_Temp_.Client.UnityAssets/copydll.bat
@@ -6,17 +6,17 @@ SET SRC_ASSETS1=%~dp0..\..\DeepMeta\DeepCore.Unity.Assets
 SET SRC_ASSETS2=%~dp0..\..\DeepMeta\DeepMetaGame.UnityAssets
 
 SET DST_ASSETS=%~dp0..\..\..\_Temp_Unity\Assets
-SET DST_RUNTIME=%~dp0..\..\..\GameEditor\_Temp_Unity\_Temp__Data\Managed
+SET DST_RUNTIME=%~dp0..\..\..\GameEditor\bin_unity\MyUnity_Data\Managed
 
 echo ----------------------------------------------------------------------------------
 echo - AOT Assets TO: %DST_ASSETS%
-xcopy /Y/E    %SRC_ASSETS1%\Assets\*    %DST_ASSETS%\
-xcopy /Y/E    %SRC_ASSETS2%\Assets\*    %DST_ASSETS%\
+xcopy /Y    %SRC_ASSETS1%\Assets\*    %DST_ASSETS%\
+xcopy /Y    %SRC_ASSETS2%\Assets\*    %DST_ASSETS%\
 
 echo ----------------------------------------------------------------------------------
 echo - AOT DLL TO: %DST_ASSETS%\Plugins\Library\
-xcopy /Y/E /EXCLUDE:%~dp0copydll.exclude.unity.txt    %TargetDir%*.dll    %DST_ASSETS%\Plugins\Library\
-xcopy /Y/E /EXCLUDE:%~dp0copydll.exclude.unity.txt    %TargetDir%*.pdb    %DST_ASSETS%\Plugins\Library\
+xcopy /Y /EXCLUDE:%~dp0copydll.exclude.unity.txt    %TargetDir%*.dll    %DST_ASSETS%\Plugins\Library\
+xcopy /Y /EXCLUDE:%~dp0copydll.exclude.unity.txt    %TargetDir%*.pdb    %DST_ASSETS%\Plugins\Library\
 
 echo ----------------------------------------------------------------------------------
 if EXIST %DST_RUNTIME% (
@@ -25,8 +25,3 @@ xcopy /Y/E /EXCLUDE:%~dp0copydll.exclude.unity.txt    %TargetDir%*.dll    %DST_R
 del /F %DST_RUNTIME%\DeepCore.UnityEditor.dll
 )
 echo ----------------------------------------------------------------------------------
-rem xcopy /Y/E /EXCLUDE:%~dp0\copydll.exclude.txt   %TargetDir%\*.dll       %~dp0\..\..\..\UnityRogueRun\Assets\Plugins\Import\
-rem xcopy /Y/E /EXCLUDE:%~dp0\copydll.exclude.txt   %TargetDir%\*.pdb       %~dp0\..\..\..\UnityRogueRun\Assets\Plugins\Import\
-rem xcopy /Y/E /EXCLUDE:%~dp0\copydll.exclude.txt   %TargetDir%\*.dll       %~dp0\..\..\..\UnityRogueArt\Assets\Plugins\Import\
-rem xcopy /Y/E /EXCLUDE:%~dp0\copydll.exclude.txt   %TargetDir%\*.pdb       %~dp0\..\..\..\UnityRogueArt\Assets\Plugins\Import\
-rem xcopy /Y/E /EXCLUDE:%~dp0\copydll.exclude.txt   %TargetDir%\*.dll       %~dp0\..\..\..\Data\GameEditor\UnityRun\RogueRuntime_Data\Managed\
